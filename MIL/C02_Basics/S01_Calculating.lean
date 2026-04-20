dc2a6c517ad80843d3f20336be4e890a11e40d54
mismorgano <mismorgano@gmail.com>
2026-04-20 00:59:14 -0600

    update: first proof using lean.
    Just using the basic  tactics: rw,
    and some basic identities mul_assoc, mul_comm, \l, and sub_self.
    
    Something to note is that the hypothesis are just proofs. which we just accept are true.
    A true fact is a proof of the fact, a tautology.

--- MIL/C02_Basics/S01_Calculating.lean
+++ MIL/C02_Basics/S01_Calculating.lean
@@ -6,11 +6,27 @@ example (a b c : ℝ) : a * b * c = b * (a * c) := by
   rw [mul_assoc b a c]
 
 -- Try these.
+-- first proof
 example (a b c : ℝ) : c * b * a = b * (a * c) := by
-  sorry
+  rw [mul_comm c b]
+  rw [mul_assoc b c a]
+  rw [mul_comm c a]
+
+-- second proof
+example (a b c : ℝ) : c * b * a = b * (a * c) := by
+  rw [mul_assoc c b a]
+  rw [mul_comm c (b * a)]
+  rw [mul_assoc b a c]
+  -- rw [mul_comm a c]
+
+-- -- third proof is redundant
+-- example (a b c : ℝ) : c * b * a = b * (a * c) := by
+--   rw [← mul_assoc]
 
 example (a b c : ℝ) : a * (b * c) = b * (a * c) := by
-  sorry
+  rw [← mul_assoc]
+  rw [mul_comm a b]
+  rw [mul_assoc]
 
 -- An example.
 example (a b c : ℝ) : a * b * c = b * c * a := by
@@ -20,10 +36,13 @@ example (a b c : ℝ) : a * b * c = b * c * a := by
 /- Try doing the first of these without providing any arguments at all,
    and the second with only one argument. -/
 example (a b c : ℝ) : a * (b * c) = b * (c * a) := by
-  sorry
+  rw [mul_comm]
+  rw [mul_assoc]
 
 example (a b c : ℝ) : a * (b * c) = b * (a * c) := by
-  sorry
+  rw [mul_comm]
+  rw [mul_assoc]
+  rw [mul_comm c]
 
 -- Using facts from the local context.
 example (a b c d e f : ℝ) (h : a * b = c * d) (h' : e = f) : a * (b * e) = c * (d * f) := by
@@ -33,10 +52,19 @@ example (a b c d e f : ℝ) (h : a * b = c * d) (h' : e = f) : a * (b * e) = c *
   rw [mul_assoc]
 
 example (a b c d e f : ℝ) (h : b * c = e * f) : a * b * c * d = a * e * f * d := by
-  sorry
+  rw [mul_assoc]
+  rw [mul_assoc]
+  rw [← mul_assoc b]
+  rw [h]
+  rw [mul_assoc]
+  rw [← mul_assoc]
+  rw [← mul_assoc]
 
 example (a b c d : ℝ) (hyp : c = b * a - d) (hyp' : d = a * b) : c = 0 := by
-  sorry
+  rw [hyp]
+  rw [mul_comm]
+  rw [hyp']
+  rw [sub_self]
 
 example (a b c d e f : ℝ) (h : a * b = c * d) (h' : e = f) : a * (b * e) = c * (d * f) := by
   rw [h', ← mul_assoc, h, mul_assoc]
