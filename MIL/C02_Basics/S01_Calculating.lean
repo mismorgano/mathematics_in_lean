1abe455c9a36a4cc7ec54a2d5fa33663c8ff9d73
mismorgano <mismorgano@gmail.com>
2026-04-22 01:48:15 -0600

    update: learn about calc keyword
    calc keyword allows us to a way to write more structured proofs .

--- MIL/C02_Basics/S01_Calculating.lean
+++ MIL/C02_Basics/S01_Calculating.lean
@@ -125,10 +125,38 @@ section
 variable (a b c d : ℝ)
 
 example : (a + b) * (c + d) = a * c + a * d + b * c + b * d := by
-  sorry
+  rw [add_mul, mul_add, mul_add]
+  rw [← add_assoc]
 
-example (a b : ℝ) : (a + b) * (a - b) = a ^ 2 - b ^ 2 := by
-  sorry
+example : (a + b) * (c + d) = a * c + a * d + b * c + b * d :=
+  calc
+    (a + b) * (c + d) = a * (c + d) + b * (c + d) := by
+      rw [add_mul]
+    _ = a * c + a * d + (b * c + b *d) := by
+      rw [mul_add, mul_add]
+    _ = a * c + a * d + b * c + b * d := by
+      rw [← add_assoc]
+
+
+example (a b : ℝ) : (a + b) * (a - b) = a ^ 2 - b ^ 2 :=
+  calc
+    (a + b) * (a - b) = (a +b) * a - (a + b) * b := by
+      rw [mul_sub]
+    _ = a * a + b *a - (a * b + b *b) := by
+      rw [add_mul, add_mul]
+    _ = a * a + (b *a - (a * b + b *b)) := by
+     rw [add_sub]
+    _ = a * a + (b * a - a*b -b *b) := by
+      rw [sub_sub]
+    _ = a * a + (a * b - a*b -b *b) := by
+      rw [mul_comm a b]
+    _ = a * a+ (0 - b*b) := by
+      rw [sub_self, ]
+     _ = a* a  + 0 - b*b := by
+      rw [add_sub]
+    _ = a ^ 2 - b^2 := by
+      rw [add_zero, pow_two, pow_two]
+
 
 #check pow_two a
 #check mul_sub a b c
