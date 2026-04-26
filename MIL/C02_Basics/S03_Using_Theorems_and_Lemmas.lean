9a5f6cf5160c92efa9930ed26348b7b65d9d6df3
mismorgano <mismorgano@gmail.com>
2026-04-26 11:12:46 -0600

    update: proving theorems and lemmas by getting used to the apply tactic
    So basically the 'apply' tactic tries to match (through pattern matching!!!) the proof with the current goal, that means
    the conclusion with the current goal, if it succeed then it introduces the hypothesis as the new goals
    of the tactic, so then we have to show that the hypothesis effectively happened (that's to say we have to prove them)
    which if fact would need more steps or maybe simply use the given hypothesis.
    
    We can also apply the 'apply' tactic with a theorem and some hypothesis thus reducing the number
    of new goals introduced.
    
    We also learn the 'linarith' tactic.
    
    Also the indentation matters, so when introduced new goals with 'apply' instead of using new indentation
    two prove that goals it keeps in the same indentation as the current 'apply', so for clearity lean allows '.' (dots)
    for explicitly show the proof for each new goal, that means that
    
    ```lean
    example (x y z : ℝ) (h₀ : x ≤ y) (h₁ : y ≤ z) : x ≤ z := by
      apply le_trans
      apply h₀
      apply h₁
    ```
    is equivalent to
    
    ```lean
    example (x y z : ℝ) (h₀ : x ≤ y) (h₁ : y ≤ z) : x ≤ z := by
      apply le_trans
      · apply h₀
      · apply h₁
    ```
    and we can see that the latter is more clear.
    
    We also learn about the 'constructor' tactic which introduces the two Props of an 'and' conjunction
    It means that to prove 'P and Q', 'constructor' allows to first prove P and the Q.

--- MIL/C02_Basics/S03_Using_Theorems_and_Lemmas.lean
+++ MIL/C02_Basics/S03_Using_Theorems_and_Lemmas.lean
@@ -42,9 +42,29 @@ example (x : ℝ) : x ≤ x :=
 #check (lt_of_lt_of_le : a < b → b ≤ c → a < c)
 #check (lt_trans : a < b → b < c → a < c)
 
--- Try this.
+-- Try this. first proof
 example (h₀ : a ≤ b) (h₁ : b < c) (h₂ : c ≤ d) (h₃ : d < e) : a < e := by
-  sorry
+  apply lt_of_le_of_lt h₀
+  -- . apply h₀
+  apply lt_trans h₁
+  exact lt_of_le_of_lt h₂ h₃
+  -- apply
+  -- apply lt_of_le_of_lt h₀ h₁
+
+-- second proof
+example (h₀ : a ≤ b) (h₁ : b < c) (h₂ : c ≤ d) (h₃ : d < e) : a < e := by
+  apply lt_trans
+  apply lt_of_le_of_lt h₀ h₁
+  exact lt_of_le_of_lt h₂ h₃
+
+-- third proof
+example (h₀ : a ≤ b) (h₁ : b < c) (h₂ : c ≤ d) (h₃ : d < e) : a < e := by
+  apply lt_of_lt_of_le
+  apply lt_of_le_of_lt h₀ h₁
+  apply le_trans h₂ -- we have to proof that d <= e, but d < e implies that
+  apply le_of_lt h₃ -- this statement is not showed but it clearly is true, and given the name convention I could guessed it.
+  -- because I need it due to the path I took to prove the theorem.
+
 
 example (h₀ : a ≤ b) (h₁ : b < c) (h₂ : c ≤ d) (h₃ : d < e) : a < e := by
   linarith
@@ -86,21 +106,34 @@ example (h₀ : a ≤ b) (h₁ : c < d) : a + exp c + e < b + exp d + e := by
     apply exp_lt_exp.mpr h₁
   apply le_refl
 
-example (h₀ : d ≤ e) : c + exp (a + d) ≤ c + exp (a + e) := by sorry
+example (h₀ : d ≤ e) : c + exp (a + d) ≤ c + exp (a + e) := by
+  apply add_le_add_left
+  apply exp_le_exp.mpr
+  apply add_le_add_left
+  exact h₀
 
 example : (0 : ℝ) < 1 := by norm_num
 
 example (h : a ≤ b) : log (1 + exp a) ≤ log (1 + exp b) := by
-  have h₀ : 0 < 1 + exp a := by sorry
+  have h₀ : 0 < 1 + exp a := by
+    apply add_pos
+    . norm_num
+    . apply exp_pos
   apply log_le_log h₀
-  sorry
+  apply add_le_add_left
+  apply exp_le_exp.mpr
+  exact h
 
 example : 0 ≤ a ^ 2 := by
   -- apply?
   exact sq_nonneg a
 
 example (h : a ≤ b) : c - exp b ≤ c - exp a := by
-  sorry
+  -- refine tsub_le_tsub ?_ ?_
+  -- apply le_refl
+  -- apply exp_le_exp.mpr
+  -- exact h
+  linarith [exp_le_exp.mpr h]
 
 example : 2*a*b ≤ a^2 + b^2 := by
   have h : 0 ≤ a^2 - 2*a*b + b^2
@@ -121,7 +154,17 @@ example : 2*a*b ≤ a^2 + b^2 := by
   linarith
 
 example : |a*b| ≤ (a^2 + b^2)/2 := by
-  sorry
+  have h : 0 ≤ a^2 - 2*a*b + b^2
+  calc
+    a^2 - 2*a*b + b^2 = (a - b)^2 := by ring
+    _ ≥ 0 := by apply pow_two_nonneg
+  have h : 0 ≤ a^2 + 2*a*b + b^2
+  calc
+    a^2 + 2*a*b + b^2 = (a + b)^2 := by ring
+    _ ≥ 0 := by apply pow_two_nonneg
+  apply abs_le'.mpr -- introduces a new goal, to prove the two inequalities implied by abs
+  constructor -- split the above 'AND' so introduces two new goals
+  linarith  -- solve first goal given h
+  linarith -- solve the second goal given the second h , but why it does not mark an error, I think there should be a name conflict between h and h
 
 #check abs_le'.mpr
-
